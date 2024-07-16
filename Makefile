adeb5318516f8e6f1ba00870b9983f05c4811298
Alan Conway <aconway@redhat.com>
2024-07-16 17:42:54 -0400

    chore: move cmd code to pkg/cmd

--- Makefile
+++ Makefile
@@ -1,43 +1,43 @@
 
-all: build lint test
+all: lint test build
 
 VERSION=0.0.3
 
-KORREL8RCLI=cmd/korrel8rcli/korrel8rcli
+include .bingo/Variables.mk
+
 VERSION_TXT=pkg/build/version.txt
 SWAGGER_SPEC=swagger.json
 SWAGGER_CLIENT=pkg/swagger
-GOCOVERDIR=cmd/korrel8rcli/covdata
-
-include .bingo/Variables.mk
-
-build: $(KORREL8RCLI)
+KORREL8RCLI=./korrel8rcli
 
 lint: $(SWAGGER_CLIENT) $(GOLANGCI_LINT)
 	go mod tidy
-	@if grep -q github.com/korrel8r/korrel8r go.mod; then echo "ERROR: go.mod contains 'github.com/korrel8r/korrel8r'; remove this dependency."; exit 1; fi
 	$(GOLANGCI_LINT) run ./...
+	@if grep -q github.com/korrel8r/korrel8r go.mod; then						\
+		echo "ERROR: bad dependency: remove 'github.com/korrel8r/korrel8r' from go.mod";	\
+		exit 1;	\
+	fi
 
-test: $(KORREL8RCLI) $(KORREL8R)
-	KORREL8R=$(KORREL8R) go test -cover -race ./...
-	@echo -e "\\nAccumulated coverage from main_test"
-	go tool covdata percent -i $(GOCOVERDIR)
+build: $(KORREL8RCLI)
+$(KORREL8RCLI): $(VERSION_TXT) $(SWAGGER_CLIENT)
+	go build -o $@  ./cmd/korrel8rcli
+
+test:
+	go test -cover -race ./...
+	go tool covdata percent -i pkg/cmd/_covdata
 
 clean:
-	rm -rf $(GOCOVERDIR) $(KORREL8RCLI) $(SWAGGER_CLIENT) $(SWAGGER_SPEC)
+	rm -rfv $(SWAGGER_CLIENT) $(SWAGGER_SPEC) $(KORREL8RCLI)
+	git clean -dfx
 
 ifneq ($(VERSION),$(file <$(VERSION_TXT)))
 .PHONY: $(VERSION_TXT) # Force update if VERSION_TXT does not match $(VERSION)
 endif
 
-$(KORREL8RCLI): $(VERSION_TXT) $(SWAGGER_CLIENT) $(shell find -name *.go)
-	@mkdir -p $(dir $@)
-	go build -cover -o $@ ./cmd/korrel8rcli
-
 $(VERSION_TXT): $(MAKEFILE_LIST)
 	echo $(VERSION) > $@
 
-$(SWAGGER_SPEC): $(KORREL8R)
+$(SWAGGER_SPEC): $(KORREL8R)	## Use bingo-installed korrel8r to generate spec.
 	 $(KORREL8R) web --spec $@
 
 $(SWAGGER_CLIENT): $(SWAGGER_SPEC) $(SWAGGER) ## Generate client packages.
--- pkg/cmd/main_test.go
+++ pkg/cmd/main_test.go
@@ -1,6 +1,6 @@
 // Copyright: This file is part of korrel8r, released under https://github.com/korrel8r/korrel8r/blob/main/LICENSE
 
-package main_test
+package cmd_test
 
 import (
 	"fmt"
@@ -8,6 +8,7 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
+	"sync"
 	"testing"
 	"time"
 
@@ -17,12 +18,12 @@ import (
 )
 
 func Test_domains(t *testing.T) {
-	u := setup(t) // Start the server
+	u := korrel8rServer(t)
 	out, err := korrel8rcli(t, "domains", "-u", u.String())
 	require.NoError(t, err)
 
 	var domains []*models.Domain
-	require.NoError(t, yaml.Unmarshal([]byte(out), &domains))
+	require.NoError(t, yaml.Unmarshal([]byte(out), &domains), out)
 	var names []string
 	for _, d := range domains {
 		names = append(names, d.Name)
@@ -31,19 +32,31 @@ func Test_domains(t *testing.T) {
 }
 
 func Test_bad_parameters(t *testing.T) {
-	u := setup(t) // Start the server
+	u := korrel8rServer(t)
 	out, err := korrel8rcli(t, "objects", "-u", u.String(), "this-is-not-a-query")
-	require.EqualError(t, err, "exit status 1: stderr: korrel8rcli: [GET /objects][400] GetObjects default {\"error\":\"invalid query string: this-is-not-a-query\"}\n")
-	require.Equal(t, out, "")
+	require.EqualError(t, err, "exit status 1: stderr: invalid query string: this-is-not-a-query\n")
+	require.Equal(t,  "", out)
 }
+
+var buildOnce sync.Once
 
 // korrel8rcli returns an exec.Cmd to run the executable in the context of a testing.T test.
 // Includes support for writing coverage data to
 func korrel8rcli(t *testing.T, args ...string) (out string, err error) {
 	t.Helper()
-	require.NoError(t, os.MkdirAll("covdata", 0770))
-	cmd := exec.Command("./korrel8rcli", args...)
-	cmd.Env = []string{"GOCOVERDIR=covdata"}
+	const (
+		dir = "../../cmd/korrel8rcli"
+		exe = "../../korrel8rcli"
+	)
+	buildOnce.Do(func() {
+		cmd := exec.Command("go", "build", "-cover", "-o", exe, dir)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		require.NoError(t, cmd.Run())
+	})
+	require.NoError(t, os.MkdirAll("_covdata", 0770))
+	cmd := exec.Command(exe, args...)
+	cmd.Env = []string{"GOCOVERDIR=_covdata"}
 	b, err := cmd.Output()
 	if exitErr, ok := err.(*exec.ExitError); ok {
 		err = fmt.Errorf("%w: stderr: %v", exitErr, string(exitErr.Stderr))
@@ -60,7 +73,7 @@ func korrel8r(t *testing.T, args ...string) *url.URL {
 	require.NoError(t, l.Close())
 	korrel8rCmd := os.Getenv("KORREL8R")
 	if korrel8rCmd == "" {
-		t.Fatal("Set env var KORREL8R to the korrel8r executable.")
+		korrel8rCmd = "korrel8r"
 	}
 	cmd := exec.Command(korrel8rCmd, "web", "--http", u.Host, "-c=testdata/korrel8r.yaml")
 	cmd.Stderr = &testWriter{Name: "korrel8r", T: t}
@@ -79,7 +92,7 @@ func (w *testWriter) Write(data []byte) (int, error) {
 	return len(data), nil
 }
 
-func setup(t *testing.T) *url.URL {
+func korrel8rServer(t *testing.T) *url.URL {
 	t.Helper()
 	u := korrel8r(t) // Start the server
 	var err error
